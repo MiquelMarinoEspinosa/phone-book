fd8ecfae92c5cafff4e7329b1c45fb86cd44cc35
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2019-12-13 10:16:55 +0100

    route the request to a controller method

---
+++ apps/controllers/PhoneBookController.php
@@ -0,0 +1,10 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class PhoneBookController extends Controller
+{
+    public static function get() {
+        echo "PhoneBookController";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,21 +1,31 @@
 <?php
 
+use Phalcon\Loader;
 use Phalcon\Mvc\Micro;
+use Phalcon\Mvc\Router;
 
 $app = new Micro();
 
+$router = new Router();
+
+$loader = new Loader();
+$loader->registerDirs(
+    [
+        "../apps/controllers/",
+        "../apps/models/",
+    ]
+);
+$loader->register();
+
+//$app->setService('router', $router, true);
+
 $app->get(
     '/test',
-    function () {
-        echo "test";
-    }
+    'PhoneBookController::get'
 );
-
-
-
 
 try {
     $app->handle();
 } catch (\Exception $exception) {
-    var_dump($_SERVER);
+    echo $exception->getMessage();
 }
